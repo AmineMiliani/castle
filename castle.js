11cc60a95d12a72c142fcc02d32b04a9a117672a
AmineMiliani <miliani.amine@orange.fr>
2019-02-12 13:59:41 +0100

    Url and name are OK

--- castle.js
+++ castle.js
@@ -5,48 +5,28 @@ const fs = require('fs');
 request('https://www.relaischateaux.com/fr/site-map/etablissements', (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
-    var info = $('#countryF').eq(1).children('ul');
-    var list = new Array();
+    var info = $('#countryF').eq(1).find('li');
+    //var list = new Array();
     info.each((i, el) => {
-      //var link = $(el).find('a').attr('href');
-      //const title = $(el).find('a').text();
-      var test = $(el).children('li');
-
-      test.each((j, el) => {
-        //var link = $(el).find('a').attr('href');
-        //const title = $(al).find('a').text();
-        var test2 = $(el).children('a');
-        test2.each((k, el) => {
-          var link = $(el).attr('href');
-          const title = $(el).text();
-
-          var hotel_Resto = new Hotel_Resto(title,link);
-
-          console.log(title);
-        });
-      //  var hotel_Resto = new Hotel_Resto(title,link);
-
-//console.log(link);
-      });
-  //    var hotel_Resto = new Hotel_Resto(title,link);
-
-//      console.log(link);
+      const link = $(el).find('a').eq(0).attr('href');
+      const title = $(el).find('a').eq(0).text();
+      const chef_name = $(el).find('a').eq(1).text();
+      var hotel_Resto = new Hotel_Resto(title,link,chef_name);
+      console.log(hotel_Resto);
     });
 
-    //  console.log(info);
-  };
-  //  console.log(info);
+}
 });
 
+    class Hotel_Resto{
 
-class Hotel_Resto{
+      constructor(name, url,chef_name){
+        this.name = name;
+        this.url = url;
+        this.chef_name = chef_name;
+      }
+      getName() {
+        return this.name;
+      }
 
-  constructor(name, url){
-    this.name = name;
-    this.url = url;
-  }
-  getName() {
-    return this.name;
-  }
-
-}
+    }
